d270c5aba1339dc5c7bc69a59bad283f47b2d750
Aveldina <95387797+Aveldina@users.noreply.github.com>
2022-01-08 20:17:19 -0600

    feat: support custom delimiters (#3)
    * tests: add custom delimiter input
    
    * feat: support custom delimiters

--- src/Service/StringCalculatorService.php
+++ src/Service/StringCalculatorService.php
@@ -6,11 +6,25 @@ class StringCalculatorService
 {
     public function add(string $input): int
     {
-        $separator = ',';
         if (strlen($input) == 0) {
             return 0;
         }
 
+        $separatorControlCheck = substr($input, 0, 2);
+        if (strcmp($separatorControlCheck, "//")  == 0) {
+            $inputData = preg_split('/\n/', $input, 2, PREG_SPLIT_NO_EMPTY);
+            $separatorString = substr($inputData[0], 2);
+            $dataString = $inputData[1];
+        }
+        else {
+            $separatorString = ',';
+            $dataString = $input;
+        }
+
+        return $this->calculateValue($dataString, $separatorString);
+    }
+
+    private function calculateValue(string $input, string $separator = ','): int {
         $inputArray = explode($separator, $input);
         $sum = 0;
         foreach ($inputArray as $item) {
