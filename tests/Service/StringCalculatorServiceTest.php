55385900aa02fe3ceeeb3acbe7cf76af6f037fb4
Aveldina <95387797+Aveldina@users.noreply.github.com>
2022-01-08 19:25:05 -0600

    feat: step 1 add function (#1)
    * test: step 1 test
    
    * feat: setup string adder function

--- tests/Service/StringCalculatorServiceTest.php
+++ tests/Service/StringCalculatorServiceTest.php
@@ -7,11 +7,41 @@ use PHPUnit\Framework\TestCase;
 
 class StringCalculatorServiceTest extends TestCase
 {
-    public function testStringCalculatorServiceSetup(): void {
-        $stringCalculatorService = new StringCalculatorService();
+    private StringCalculatorService $service;
 
-        $result = $stringCalculatorService->outputTest();
+    protected function setUp(): void
+    {
+        $this->service = new StringCalculatorService();
+    }
+
+    public function testServiceSetup(): void
+    {
+        $result = $this->service->outputTest();
         $this->assertIsString($result);
     }
 
+    /**
+     * @dataProvider addReturnsExpectedProvider
+     */
+    public function testAddReturnsExpected(string $input, int $expected): void
+    {
+        $result = $this->service->add($input);
+
+        $this->assertIsInt($result);
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @return array<int, array<int, mixed>>
+     */
+    public function addReturnsExpectedProvider(): array
+    {
+        return [
+            ["1,2,5", 8],
+            ["", 0],
+            ["5,10,5", 20],
+            ["999,1", 1000],
+            ["2,2,2,2,2,2,2,2,2,2,2,2", 24]
+        ];
+    }
 }
